12c408c65933a0559fd48d4a4bba941e78806263
Istvan Krucsanyica <istvan.krucsanyica@gmail.com>
2017-03-20 08:43:51 +0100

    Javítva a debug bar megjelenítése és információ tartalma

--- show-debugbar.php
+++ show-debugbar.php
@@ -7,7 +7,7 @@
    * @copyright (c) 2017, GNUv2
    * @package KVwpBaseFunctions
    * @since 1.0.2
-   * @version 1.0.2
+   * @version 1.0.3
    */
 
 
@@ -30,7 +30,7 @@ class KVBF_Debug_Bar {
   public function kvbf_style() {
     echo '
     <style type="text/css">
-      #kvbf-debug-box{color:#8B0000;background:#FA8072;padding:1em 1em 1em 1.5em;position:fixed;bottom:0px;right:0px;font-family:monospace;font-size:13px;}
+      #kvbf-debug-box{color:#8B0000;background:#FA8072;padding:1em 1em 1em 1.5em;position:fixed;bottom:0px;left:0px;z-index:9999;font-family:monospace;font-size:13px;}
       #kvbf-debug-box ul{list-style:none outside;margin:0;padding:0;}
       #kvbf-debug-box ul li{list-style:none outside;margin:0;padding:0;line-height:1.5em;}
     </style>';
@@ -41,13 +41,16 @@ class KVBF_Debug_Bar {
     $query_count        = get_num_queries();
     $memory_usage       = $this->convert( memory_get_usage() );
     $memory_peak_usage  = $this->convert( memory_get_peak_usage() );
-    $memory_limit       = $this->convert( $this->let_to_num( WP_MEMORY_LIMIT ) );
+    $memory_limit       = $this->convert( $this->let_to_num( ini_get('memory_limit') ) );
+    $wpmemory_limit     = $this->convert( $this->let_to_num( WP_MEMORY_LIMIT ) );
     ?>
     <div id="kvbf-debug-box">
       <ul>
-        <li><?php printf( '%s lekérdezés %s másodperc alatt.', $query_count, $timer_stop ); ?></li>
+        <li><?php printf( '%s lekérdezés %s másodperc alatt', $query_count, $timer_stop ); ?></li>
         <li><?php printf( '%s-ból %s felhasználva (%s)', $memory_limit, $memory_usage, round( ( $memory_usage / $memory_limit ), 2 ) * 100 . '%' ); ?></li>
-        <li><?php printf( 'Legnagyobb memória használat: %s.', $memory_peak_usage ); ?></li>
+        <li><?php printf( 'Legnagyobb memória használat: %s', $memory_peak_usage ); ?></li>
+        <li><?php printf( 'memory_limit: %s (php.ini)', $memory_limit ); ?></li>
+        <li><?php printf( 'WP_MEMORY_LIMIT: %s (wp-config.php)', $wpmemory_limit ); ?></li>
       </ul>
     </div>
     <?php
